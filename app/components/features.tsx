c51a6d1e907abf9e587d1160ff80edad2031f365
Francisco <franciscomuniozs@gmail.com>
2025-02-12 13:58:48 -0300

    Added code block into card component

--- app/components/features.tsx
+++ app/components/features.tsx
@@ -3,8 +3,28 @@
 import React from "react"
 import { WobbleCard } from "./ui/wobble-card"
 import { TextGenerateEffect } from "./ui/text-generated-effect"
+import { CodeBlock } from "./ui/code-block"
 
 const Features = () => {
+  const code = `const DummyComponent = () => {
+    const [count, setCount] = React.useState(0);    
+    const handleClick = () => {
+      setCount(prev => prev + 1);
+    };
+      return (
+        <div className="p-4 border rounded-lg">
+          <h2 className="text-xl font-bold mb-4">Fights Counter</h2>
+          <p className="mb-2">Fight Club Fights Count: {count}</p>
+          <button 
+            onClick={handleClick}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            Increment
+          </button>
+        </div>
+      );
+    };
+  `;
   return (
     <div>
       <div className="flex justify-center relative my-20 z-10">
@@ -26,6 +46,14 @@ const Features = () => {
               brindando soluciones a medida en desarrollo web y backend, 
               adaptándome a diversos sectores y necesidades de clientes.
             </p>
+          </div>
+          <div className="absolute -right-4 lg:-right-[10%] lg:top-[15%] filter -bottom-20 object-contain rounded-2xl">
+            <CodeBlock
+              language="jsx"
+              filename="DummyComponent.jsx"
+              highlightLines={[9, 13, 14, 18]}
+              code={code}
+            />
           </div>
         </WobbleCard>
         <WobbleCard
